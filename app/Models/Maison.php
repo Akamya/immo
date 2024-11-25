9185e1f8deb092f0831ee8a57bdbff375c4a9e4c
Akamya <elodie.langlet@gmail.com>
2024-11-25 14:57:59 +0100

    init laravel intertia vue

---
+++ app/Models/Maison.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Maison extends Model
+{
+    /** @use HasFactory<\Database\Factories\MaisonFactory> */
+    use HasFactory;
+}
